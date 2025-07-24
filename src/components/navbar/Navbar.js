9823e974332ad3630ed6e9f118ad271c7c763187
d-pandian <dpandian016@gmail.com>
2025-07-24 18:22:44 +0530

    Updated Experience, Achievement, and Navbar components

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -45,9 +45,8 @@ const Navbar = () => {
               <div>
                 <img className="w-32" src={logo} alt="logo" />
                 <p className="text-sm text-gray-400 mt-2">
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                  Earum soluta perspiciatis molestias enim cum repellat, magnam
-                  exercitationem distinctio aliquid nam.
+                    Passionate MERN Stack Developer | Building Scalable Web Applications with MongoDB, Express, React, and Node.js
+
                 </p>
               </div>
               <ul className="flex flex-col gap-4">
